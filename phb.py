88dde64073f5fb96e361cd72ca804f78f4b41423
dido11 <v_m_1732@orange.fr>
2020-05-27 18:59:59 +0200

    Changed styles

--- phb.py
+++ phb.py
@@ -1,5 +1,6 @@
 import discord
-import token
+import tok
+import random
 
 class Bot(discord.Client):
     def __init__(self):
@@ -14,11 +15,21 @@ class Bot(discord.Client):
         if (message.author == self.user):
             return
 
-        text = str(message.content == 'test')
-        message.channel.send('Test !')
+        text = str(message.content)
+        if "elle" in text.lower():
+            if "encore" in text.lower():
+                if random.randint(0,4)!=0:
+                    return
+
+            tab=['**Elle a pas encore mangé !**',
+                    '__**ELLE A PAS ENCORE MANGÉ !**__',
+                    '**eLlE a PaS eNcOrE mAnGé !**',
+                    '**3773 A PA5 3NC0R3 MANG3 !**',
+                    '**ELLE** a pas __encore__ mangé...']
+            await message.channel.send(random.choice(tab))
 
 if __name__ == "__main__":
 
     bot = Bot()
 
-bot.run(TOKEN)
+bot.run(tok.TOKEN)
